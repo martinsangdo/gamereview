879fdf70a15e50daa8df1b37f1fed3ccde68cfee
Martin SangDo <martin.sangdo@gmail.com>
2018-03-22 19:18:12 +0700

    - [home]center image

--- application/views/front/webview/home.php
+++ application/views/front/webview/home.php
@@ -60,144 +60,14 @@
                         <form class="input-group rounded">
                             <input class="form-control w-100 g-brd-secondary-light-v2 g-brd-primary--focus g-color-secondary-dark-v1 g-placeholder-secondary-dark-v1 g-bg-white g-font-weight-400 g-font-size-13 rounded g-px-20 g-py-12" type="text" placeholder="Search the entire site">
                             <span class="input-group-addon g-brd-none g-py-0 g-pr-0">
-                    <button class="btn u-btn-white g-color-primary--hover g-bg-secondary g-font-weight-600 g-font-size-13 text-uppercase rounded g-py-12 g-px-20" type="submit">
-                      <span class="g-hidden-md-down">Search</span>
-                      <i class="g-hidden-lg-up fa fa-search"></i>
-                    </button>
-                  </span>
+                            <button class="btn u-btn-white g-color-primary--hover g-bg-secondary g-font-weight-600 g-font-size-13 text-uppercase rounded g-py-12 g-px-20" type="submit">
+                              <span class="g-hidden-md-down">Search</span>
+                              <i class="g-hidden-lg-up fa fa-search"></i>
+                            </button>
+                          </span>
                         </form>
                     </div>
                     <!-- End Subscribe Form -->
-
-                    <!-- Language -->
-                    <div class="col-4 col-lg-2 g-pos-rel g-px-15 ml-auto">
-                        <a id="languages-dropdown-invoker" class="g-color-secondary-dark-v1 g-color-primary--hover g-text-underline--none--hover" href="#!"
-                           aria-controls="languages-dropdown"
-                           aria-haspopup="true"
-                           aria-expanded="false"
-                           data-dropdown-event="hover"
-                           data-dropdown-target="#languages-dropdown"
-                           data-dropdown-type="css-animation"
-                           data-dropdown-duration="300"
-                           data-dropdown-hide-on-scroll="false"
-                           data-dropdown-animation-in="fadeIn"
-                           data-dropdown-animation-out="fadeOut">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="11" width="27" viewBox="0 0 640 480">
-                                <defs>
-                                    <clipPath id="a">
-                                        <path fill-opacity=".67" d="M-85.333 0h682.67v512h-682.67z"/>
-                                    </clipPath>
-                                </defs>
-                                <g clip-path="url(#a)" transform="translate(80) scale(.94)">
-                                    <g stroke-width="1pt">
-                                        <path fill="#006" d="M-256 0H768.02v512.01H-256z"/>
-                                        <path d="M-256 0v57.244l909.535 454.768H768.02V454.77L-141.515 0H-256zM768.02 0v57.243L-141.515 512.01H-256v-57.243L653.535 0H768.02z" fill="#fff"/>
-                                        <path d="M170.675 0v512.01h170.67V0h-170.67zM-256 170.67v170.67H768.02V170.67H-256z" fill="#fff"/>
-                                        <path d="M-256 204.804v102.402H768.02V204.804H-256zM204.81 0v512.01h102.4V0h-102.4zM-256 512.01L85.34 341.34h76.324l-341.34 170.67H-256zM-256 0L85.34 170.67H9.016L-256 38.164V0zm606.356 170.67L691.696 0h76.324L426.68 170.67h-76.324zM768.02 512.01L426.68 341.34h76.324L768.02 473.848v38.162z" fill="#c00"/>
-                                    </g>
-                                </g>
-                            </svg>
-                            <span>English</span> <i class="g-hidden-sm-down fa fa-angle-down g-ml-7"></i>
-                        </a>
-
-                        <ul id="languages-dropdown" class="list-unstyled g-width-160 g-brd-around g-brd-secondary-light-v2 g-bg-white rounded g-pos-abs g-py-5 g-mt-32"
-                            aria-labelledby="languages-dropdown-invoker">
-                            <li>
-                                <a class="d-block g-color-secondary-dark-v1 g-color-primary--hover g-text-underline--none--hover g-py-5 g-px-20" href="#!">
-                                    <svg class="mr-1 g-ml-minus-6" xmlns="http://www.w3.org/2000/svg" height="11" width="27" viewBox="0 0 640 480">
-                                        <defs>
-                                            <clipPath id="a">
-                                                <path fill-opacity=".67" d="M-85.333 0h682.67v512h-682.67z"/>
-                                            </clipPath>
-                                        </defs>
-                                        <g clip-path="url(#a)" transform="translate(80) scale(.94)">
-                                            <g stroke-width="1pt">
-                                                <path fill="#006" d="M-256 0H768.02v512.01H-256z"/>
-                                                <path d="M-256 0v57.244l909.535 454.768H768.02V454.77L-141.515 0H-256zM768.02 0v57.243L-141.515 512.01H-256v-57.243L653.535 0H768.02z" fill="#fff"/>
-                                                <path d="M170.675 0v512.01h170.67V0h-170.67zM-256 170.67v170.67H768.02V170.67H-256z" fill="#fff"/>
-                                                <path d="M-256 204.804v102.402H768.02V204.804H-256zM204.81 0v512.01h102.4V0h-102.4zM-256 512.01L85.34 341.34h76.324l-341.34 170.67H-256zM-256 0L85.34 170.67H9.016L-256 38.164V0zm606.356 170.67L691.696 0h76.324L426.68 170.67h-76.324zM768.02 512.01L426.68 341.34h76.324L768.02 473.848v38.162z" fill="#c00"/>
-                                            </g>
-                                        </g>
-                                    </svg>
-                                    English
-                                </a>
-                            </li>
-                            <li>
-                                <a class="d-block g-color-secondary-dark-v1 g-color-primary--hover g-text-underline--none--hover g-py-5 g-px-20" href="#!">
-                                    <svg class="mr-1 g-ml-minus-6" xmlns="http://www.w3.org/2000/svg" height="11" width="27" viewBox="0 0 640 480">
-                                        <g stroke-width="1pt" fill-rule="evenodd">
-                                            <path fill="#fff" d="M0 0h640v480H0z"/>
-                                            <path fill="#00267f" d="M0 0h213.33v480H0z"/>
-                                            <path fill="#f31830" d="M426.67 0H640v480H426.67z"/>
-                                        </g>
-                                    </svg>
-                                    Spanish
-                                </a>
-                            </li>
-                            <li>
-                                <a class="d-block g-color-secondary-dark-v1 g-color-primary--hover g-text-underline--none--hover g-py-5 g-px-20" href="#!">
-                                    <svg class="mr-1 g-ml-minus-6" xmlns="http://www.w3.org/2000/svg" height="11" width="27" viewBox="0 0 640 480">
-                                        <g fill-rule="evenodd" stroke-width="1pt">
-                                            <path fill="#fff" d="M0 0h640v480H0z"/>
-                                            <path fill="#0039a6" d="M0 160.003h640V480H0z"/>
-                                            <path fill="#d52b1e" d="M0 319.997h640V480H0z"/>
-                                        </g>
-                                    </svg>
-                                    Russian
-                                </a>
-                            </li>
-                            <li>
-                                <a class="d-block g-color-secondary-dark-v1 g-color-primary--hover g-text-underline--none--hover g-py-5 g-px-20" href="#!">
-                                    <svg class="mr-1 g-ml-minus-6" xmlns="http://www.w3.org/2000/svg" height="11" width="27" viewBox="0 0 640 480">
-                                        <path fill="#ffce00" d="M0 320h640v160.002H0z"/>
-                                        <path d="M0 0h640v160H0z"/>
-                                        <path fill="#d00" d="M0 160h640v160H0z"/>
-                                    </svg>
-                                    German
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                    <!-- End Language -->
-
-                    <!-- Account -->
-                    <div class="col-1">
-                        <a id="account-dropdown-invoker" class="media align-items-center float-right g-text-underline--none--hover" href="#!"
-                           aria-controls="account-dropdown"
-                           aria-haspopup="true"
-                           aria-expanded="false"
-                           data-dropdown-event="hover"
-                           data-dropdown-target="#account-dropdown"
-                           data-dropdown-type="css-animation"
-                           data-dropdown-duration="300"
-                           data-dropdown-hide-on-scroll="false"
-                           data-dropdown-animation-in="fadeIn"
-                           data-dropdown-animation-out="fadeOut">
-                            <div class="d-flex g-width-30 g-height-30 mr-2">
-                                <img class="img-fluid rounded-circle" src="/public/unity_assets/img-temp/100x100/img11.jpg" alt="Image Description">
-                            </div>
-                            <div class="media-body">
-                                <span class="d-block g-hidden-sm-down g-color-main g-font-weight-600 g-font-size-13">Martin</span>
-                            </div>
-                        </a>
-
-                        <ul id="account-dropdown" class="list-unstyled text-right g-width-160 g-brd-around g-brd-secondary-light-v2 g-bg-white rounded g-pos-abs g-right-0 g-py-5 g-mt-57"
-                            aria-labelledby="account-dropdown-invoker">
-                            <li>
-                                <a class="d-block g-color-secondary-dark-v1 g-color-primary--hover g-text-underline--none--hover g-py-5 g-px-20" href="#!">My Account</a>
-                            </li>
-                            <li>
-                                <a class="d-block g-color-secondary-dark-v1 g-color-primary--hover g-text-underline--none--hover g-py-5 g-px-20" href="#!">Notifications</a>
-                            </li>
-                            <li>
-                                <a class="d-block g-color-secondary-dark-v1 g-color-primary--hover g-text-underline--none--hover g-py-5 g-px-20" href="#!">Settings</a>
-                            </li>
-                            <li>
-                                <a class="d-block g-color-secondary-dark-v1 g-color-primary--hover g-text-underline--none--hover g-py-5 g-px-20" href="#!">Signout</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <!-- End Account -->
                 </div>
             </div>
         </div>
@@ -1060,7 +930,9 @@
                     <!-- Article -->
                     <article class="u-block-hover">
                         <figure class="u-shadow-v25 g-bg-cover g-bg-white-gradient-opacity-v1--after">
-                            <img class="img-fluid w-100 u-block-hover__main--zoom-v1" src="<?php echo $block_key_1[0]->thumb_url;?>" alt="Image Description">
+                            <div class="home1-center-cropped"
+                                 style="background-image: url('<?php echo $block_key_1[0]->thumb_url;?>');">
+                            </div>
                         </figure>
 
                         <span class="g-hidden-xs-down g-pos-abs g-top-30 g-left-30">
@@ -1090,7 +962,9 @@
                     <!-- Article -->
                     <article class="u-block-hover">
                         <figure class="u-shadow-v25 g-bg-cover g-bg-white-gradient-opacity-v1--after">
-                            <img class="img-fluid w-100 u-block-hover__main--zoom-v1" src="<?php echo $block_key_1[1]->thumb_url;?>" alt="Image Description">
+                            <div class="home1-center-cropped"
+                                 style="background-image: url('<?php echo $block_key_1[1]->thumb_url;?>');">
+                            </div>
                         </figure>
 
                         <span class="g-hidden-xs-down g-pos-abs g-top-30 g-left-30">
@@ -1120,7 +994,9 @@
                     <!-- Article -->
                     <article class="u-block-hover">
                         <figure class="u-shadow-v25 u-bg-overlay g-bg-white-gradient-opacity-v1--after">
-                            <img class="u-block-hover__main--zoom-v1 img-fluid w-100" src="<?php echo $block_key_1[2]->thumb_url;?>" alt="Image Description">
+                            <div class="home2-center-cropped"
+                                 style="background-image: url('<?php echo $block_key_1[2]->thumb_url;?>');">
+                            </div>
                         </figure>
 
                         <div class="w-100 text-center g-absolute-centered g-px-30">
@@ -1140,7 +1016,9 @@
                     <!-- Article -->
                     <article class="u-block-hover">
                         <figure class="u-shadow-v25 u-bg-overlay g-bg-white-gradient-opacity-v1--after">
-                            <img class="u-block-hover__main--zoom-v1 img-fluid w-100" src="<?php echo $block_key_1[3]->thumb_url;?>" alt="Image Description">
+                            <div class="home2-center-cropped"
+                                 style="background-image: url('<?php echo $block_key_1[3]->thumb_url;?>');">
+                            </div>
                         </figure>
 
                         <div class="w-100 text-center g-absolute-centered g-px-30">
@@ -1160,7 +1038,9 @@
                     <!-- Article -->
                     <article class="u-block-hover">
                         <figure class="u-shadow-v25 u-bg-overlay g-bg-white-gradient-opacity-v1--after">
-                            <img class="u-block-hover__main--zoom-v1 img-fluid w-100" src="<?php echo $block_key_1[4]->thumb_url;?>" alt="Image Description">
+                            <div class="home2-center-cropped"
+                                 style="background-image: url('<?php echo $block_key_1[4]->thumb_url;?>');">
+                            </div>
                         </figure>
 
                         <div class="w-100 text-center g-absolute-centered g-px-30">
